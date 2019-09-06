8e03e3e80fa82135ca7015c33b89e60aeae0efa4
Diney <diney.dom@gmail.com>
2019-09-06 15:15:05 -0300

    Projeto foi criado

---
+++ src/main/java/com/lojamari/LojamariApplication.java
@@ -0,0 +1,13 @@
+package com.lojamari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojamariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojamariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojamari/LojamariApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lojamari;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LojamariApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
